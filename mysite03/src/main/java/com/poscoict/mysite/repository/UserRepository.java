07169b7b459a1eaf8d5c39f908c637006d055b3a
UijinJung <tkrk1226@ajou.ac.kr>
2022-01-25 15:49:34 +0900

    mysite03 #MyBatis-UserRepository [220125-1549]

--- mysite03/src/main/java/com/poscoict/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/poscoict/mysite/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package com.poscoict.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.sql.DataSource;
-
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,225 +12,31 @@ import com.poscoict.mysite.vo.UserVo;
 
 @Repository
 public class UserRepository {
+	
 	@Autowired
-	private DataSource dataSource;
+	private SqlSession sqlSession;
+	
 	
 	public boolean insert(UserVo vo) {
-		
-		boolean result = false;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			//3. SQL 준비
-			String sql = "insert into user values(null, ?, ?, ?, ?, now())";
-			pstmt = conn.prepareStatement(sql);
-
-			//4. 바인딩(binding)	
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-			
-			//5. SQL 실행 , executeQuery는 rs, executeUpdate는 int로 반환한다. 
-			result = (pstmt.executeUpdate() == 1);
-			
-			//boiler plate code => 상투적인 코드 => 비효율 
-						
-		} catch (SQLException e) {
-			System.out.print("error : " + e); // e.getMessage()
-		}
-		
-		finally {
-			// 자원 정리 -> try OR catch 둘 다 실행 
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
+		// sql문의 ID
+		int count = sqlSession.insert("user.insert", vo);
+		return count == 1;
 	}
 
 	public UserVo findByEmailAndPassword(String email, String password) throws UserRepositoryException {
 		// 처음에는 null 이었지만 select에 있다면 제대로 된 객체를 반환하도록
-		UserVo result = null;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			conn = dataSource.getConnection();
-			
-			//3. SQL 준비
-			String sql = "select no, name from user where email = ? and password = ?";
-			pstmt = conn.prepareStatement(sql);
-
-			//4. 바인딩(binding)	
-			pstmt.setString(1, email);
-			pstmt.setString(2, password);
-			
-			//5. SQL 실행 , executeQuery는 rs, executeUpdate는 int로 반환한다. 
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
-				 Long no = rs.getLong(1);
-				 String name = rs.getString(2);
-				 result = new UserVo();
-				 result.setNo(no);
-				 result.setName(name);
-			}
-			//boiler plate code => 상투적인 코드 => 비효율 
-		} catch (SQLException e) {
-			throw new UserRepositoryException(e.toString());
-//			System.out.print("error : " + e); // e.getMessage()
-		}
-		
-		finally {
-			// 자원 정리 -> try OR catch 둘 다 실행 
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
+		Map<String, String> map = new HashMap<>();
+		map.put("e", email);
+		map.put("p", password);
+		return sqlSession.selectOne("findByEmailAndPassword", map);
 	}
 	
 	public boolean update(UserVo vo) {
-		
-		boolean result = false;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			//3. SQL 준비
-			
-			String sql;
-			
-			if(vo.getPassword() == null || "".equals(vo.getPassword())) {
-				sql = "update user set name = ?, gender = ? where no = ?";
-			} else {
-				sql = "update user set name = ?, gender = ?, password = ? where no = ?";
-			}
-			
-			pstmt = conn.prepareStatement(sql);
-
-			//4. 바인딩(binding)	
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getGender());
-			if(vo.getPassword() == null || "".equals(vo.getPassword())) {
-				pstmt.setLong(3, vo.getNo());
-			}
-			else {
-				pstmt.setString(3, vo.getPassword());
-				pstmt.setLong(4, vo.getNo());
-			}
-			
-			//5. SQL 실행 , executeQuery는 rs, executeUpdate는 int로 반환한다. 
-			result = (pstmt.executeUpdate() == 1);
-			
-			//boiler plate code => 상투적인 코드 => 비효율 
-						
-		} catch (SQLException e) {
-			System.out.print("error : " + e); // e.getMessage()
-		}
-		
-		finally {
-			// 자원 정리 -> try OR catch 둘 다 실행 
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
+		int count = sqlSession.update("user.update", vo);
+		return count == 1;
 	}
 
 	public UserVo findByNo(Long userNo) {
-
-		// 처음에는 null 이었지만 select에 있다면 제대로 된 객체를 반환하도록
-		UserVo result = null;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			//3. SQL 준비
-			String sql = "select no, name, email, gender from user where no = ?";
-			
-			//4. 바인딩(binding)	
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setLong(1, userNo);
-			
-			
-			//5. SQL 실행 , executeQuery는 rs, executeUpdate는 int로 반환한다. 
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
-				 Long no = rs.getLong(1);
-				 String name = rs.getString(2);
-				 String email = rs.getString(3);
-				 String gender = rs.getString(4);
-				 
-				 result = new UserVo();
-				 result.setNo(no);
-				 result.setName(name);
-				 result.setEmail(email);
-				 result.setGender(gender);
-			}
-			
-			//boiler plate code => 상투적인 코드 => 비효율 
-						
-		} catch (SQLException e) {
-			System.out.print("error : " + e); // e.getMessage()
-		}
-		
-		finally {
-			// 자원 정리 -> try OR catch 둘 다 실행 
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
+		return sqlSession.selectOne("user.findByNo", userNo);
 	}
 }
