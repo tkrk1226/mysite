6c77bf62b561a268914bc697c6fbd856d57b5517
UijinJung <tkrk1226@ajou.ac.kr>
2022-02-03 13:07:31 +0900

    mysite03 #admin page, role [220203-1307]

--- mysite03/src/main/java/com/poscoict/mysite/vo/UserVo.java
+++ mysite03/src/main/java/com/poscoict/mysite/vo/UserVo.java
@@ -24,7 +24,14 @@ public class UserVo {
 	
 	private String gender;
 	private String joinDate;
-
+	private String role;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 	public Long getNo() {
 		return no;
 	}
@@ -65,6 +72,6 @@ public class UserVo {
 	@Override
 	public String toString() {
 		return "UserVo [no=" + no + ", name=" + name + ", email=" + email + ", password=" + password + ", gender="
-				+ gender + ", joinDate=" + joinDate + "]";
+				+ gender + ", joinDate=" + joinDate + ", role=" + role + "]";
 	}	
 }
