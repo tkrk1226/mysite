a844c4d963e69b4a8533b0eeb0c344c0add7226e
UijinJung <tkrk1226@ajou.ac.kr>
2022-02-03 17:38:54 +0900

    mysite03 # admin Main update complete , interceptor -ing... [220203-1738]

---
+++ mysite03/src/main/java/com/poscoict/mysite/interceptor/SiteInterceptor.java
@@ -0,0 +1,9 @@
+package com.poscoict.mysite.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SiteInterceptor extends HandlerInterceptorAdapter {
+
+	
+	
+}
