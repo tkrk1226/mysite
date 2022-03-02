5126e6f98781e2645d7b6466d0bb47140c6d1bb0
UijinJung <tkrk1226@ajou.ac.kr>
2022-03-02 11:13:49 +0900

    mysite05 # apply joinForm [220302-1113]

--- mysite05/src/main/java/com/poscoict/config/web/MvcConfig.java
+++ mysite05/src/main/java/com/poscoict/config/web/MvcConfig.java
@@ -1,7 +1,17 @@
 package com.poscoict.config.web;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -25,8 +35,47 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	}
 
 	// Message Converter (오늘은 생략, ajax할 때..)
+	@Bean
+	public StringHttpMessageConverter stringHttpMessageConverter() {
+		StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+		
+//		List<MediaType> list = new ArrayList<>();
+//		list.add(new MediaType("text", "html", Charset.forName("utf-8")));
+//		messageConverter.setSupportedMediaTypes(list);
+//		array에 담아준다. Arrays.asList("", "", "", "");
+		
+		messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+						new MediaType("text", "html", Charset.forName("utf-8"))
+						)		
+		);	
+		return messageConverter;
+	}
 	
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
+				.indentOutput(true) // jsonForm indent를 위함
+				.dateFormat(new SimpleDateFormat("yyyy-mm-dd")); // converter를 위함
+		
+		// converter에 mapper가 있어야하므로 build 형식으로 넣어줘야한다.
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build());
+		
+		messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+						new MediaType("application", "json", Charset.forName("utf-8"))
+						)		
+		);	
 	
+		return messageConverter;
+	}
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(stringHttpMessageConverter());
+		converters.add(mappingJackson2HttpMessageConverter());
+	}
+
 	// DefaultServlet Handler
 //	@Override
 //	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
--- mysite05/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
+++ mysite05/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.poscoict.mysite.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poscoict.mysite.dto.JsonResult;
 
 // AOP
 @ControllerAdvice
@@ -16,7 +22,10 @@ public class GlobalExceptionHandler {
 	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
-	public String ExceptionHandler(Model model, Exception e) {
+	public void ExceptionHandler(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Exception e) throws Exception {
 		// 1. 로깅
 		// 나중에 String으로 뽑아서 파일로 저장해야한다.
 		// 혹은 서비스 론칭 전에 개발용으로 할때는 
@@ -27,11 +36,36 @@ public class GlobalExceptionHandler {
 		// 추후 로깅을 위함
 		LOGGER.error(errors.toString()); 
 		
-		// 화면에 뿌리던 내용 다 가진 상태
-		model.addAttribute("exception", errors.toString());
+		// 2. 요청 구분
+		// JSON 요청인 경우 : request header의 accept에 application/json
+		// HTML 요청인 경우 : request header의 accept에 text/html
 		
-		// 2. 사과 페이지 (HTML 응답, 정상 종료) 
-		return "error/exception";
+		String accept = request.getHeader("accept");
+		
+		if(accept.matches(".*application/json.*")) {
+			// 3. JSON 응답
+			JsonResult result = JsonResult.fail(errors.toString());
+			
+			// error내용을 메세지에 담아서 전달
+			
+			// import com.fasterxml.jackson.databind.ObjectMapper;
+			String jsonString = new ObjectMapper().writeValueAsString(result); 
+			response.setContentType("application/json; charset=UTF-8");
+			
+			// write 대신에 Byte단위로 쓴다.
+			OutputStream os = response.getOutputStream(); // Byte 단위 Stream 따라서 각 관계간 decode, encode가 필요하다.
+			os.write(jsonString.getBytes("utf-8"));
+			os.close();
+			
+		} else {
+			// 4. 사과페이지(HTML 응답, 정상종료)
+			request.setAttribute("exception", errors.toString());
+			request
+				.getRequestDispatcher("/WEB-INF/views/error/exception.jsp")
+				.forward(request, response);
+			//model.addAttribute("exception", errors.toString());
+			//return "error/exception";
+		}
 	}
 }
 
